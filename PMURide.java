870f653233c9d527c22bfbd4f62764a852f298ec
hercu <alhussain.ali2114@gmail.com>
2023-12-16 22:34:47 +0300

    Created changePassowrd function for currentuser

--- PMURide.java
+++ PMURide.java
@@ -1,5 +1,32 @@
-public class PMURide {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class PMURide {
+    private static UserAccount currentUser = null;
+    private static List<UserAccount> userAccountsData = new ArrayList<>();
+
+    public static void main(String[] args) {
+        // Your existing main method code...
+    }
+
+    public static void changePassword(String newPassword) {
+        if (currentUser == null) {
+            System.out.println("No user is currently logged in.");
+        } else {
+            currentUser.setPassword(newPassword);
+            System.out.println("Password changed successfully for " + currentUser.getEmail());
+        }
+    }
+
+    public static void printWelcomeMessage() {
+        int header = 29;
+
+        for (int i = 0; i <= header; i++) {
+            if (header/2 == i) {
+                System.out.print("Welcome To PMU Ride");
+            }
+            System.out.print('*');
+        }
+        System.out.println(); // Print a new line for formatting
     }
 }
