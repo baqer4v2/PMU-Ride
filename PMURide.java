4cd2c290e75bc2aba5e50db27d8e0edb6f069022
baqer4v2 <baqer4@gmail.com>
2023-12-16 22:21:34 +0300

    Created a function printWelcomeMessage

--- PMURide.java
+++ PMURide.java
@@ -1,5 +1,17 @@
 public class PMURide {
     public static void main(String[] args) {
 
+
     }
-}
+
+    public static void printWelcomeMessage() {
+        int header = 29;
+
+        for (int i = 0; i <= header; i++) {
+            if (header/2 == i) {
+                System.out.print("Welcome To PMU Ride");
+            }
+            System.out.print('*');
+           }
+        }
+    }
