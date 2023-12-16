89b1455cb629ffcbb332d641839c763408eb64d3
baqer4v2 <baqer4@gmail.com>
2023-12-16 21:51:50 +0300

    Updated UserAccount Class

--- UserAccount.java
+++ UserAccount.java
@@ -1,2 +1,25 @@
 public class UserAccount {
+    private String email;
+    private String password;
+
+    public UserAccount(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
